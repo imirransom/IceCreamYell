0ae949a2c90cb4e9460bb0296479ac105df4b4a3
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-14 23:05:11 -0500

    trying to get gender column to say male and female

--- ice_cream_reader.py
+++ ice_cream_reader.py
@@ -1,7 +1,22 @@
 from IPython.display import display
+from tabulate import tabulate
 import pandas as pd
 
-icdb = pd.read_csv("ice_cream.csv")
-ic = pd.DataFrame(icdb)
+ic = pd.read_csv("ice_cream.csv")
+ic.columns = ["person_id", "gender", "ice_cream_flavor", "video_game", "puzzle_game"]
 
-print(ic.style)
+if 0 in ic.gender:
+    ic["gender"] = "Male"
+if 1 in ic.gender:
+    ic["gender"] = "Female"
+
+icdf = pd.DataFrame(ic)
+
+print(tabulate(icdf, headers="keys", tablefmt="psql"))
+
+unpivot_ic = ic.groupby(["gender", "ice_cream_flavor"]) ["video_game"]
+
+# pivoted_ic = ic.pivot(columns="ice_cream",
+#                       index=
+#
+#                       )
