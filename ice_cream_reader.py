aee5e18d2113fd2d030006dfde04ae4dbc1ce435
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-15 14:49:18 -0500

    made a table with average video game scores

--- ice_cream_reader.py
+++ ice_cream_reader.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 # Reading the CSV file using the pandas module
 ic = pd.read_csv("ice_cream.csv")
+print(ic.head(20))
 # print(ic.head(10))
 # Changed all column names using the column method so that they are more clear
 ic.columns = ["person_id", "gender", "ice_cream_flavor", "video_game_score", "puzzle_game_score"]
@@ -20,20 +21,29 @@ print("-------------------------------------------------\n")
 # Using the tabulate library that takes a Pandas DataFrame as the first parameter, the second parameter the header: it
 # takes the column names (keys) and uses them as headers in the table and the third parameter specifies the format
 # of the table which is set to "psql"
+
+# I made a tabulate function that will take a dataframe and table format as a parameter
 def to_tabulate(df, format):
     print(tabulate(df, headers="keys", tablefmt=format))
 
 # print(tabulate(icdf.head(20), headers="keys", tablefmt="psql"))
 
 
-to_tabulate(icdf.head(20), "psql")
+to_tabulate(icdf.head(20), "you_track")
 
+# This will group up the columns gender and game scores and show the average of how males and females differ
+print("\nAVERAGE PUZZLE GAME SCORE\n")
+print("-------------------------------------------------")
+puzzle_ic_by_gender = ic.groupby("gender").puzzle_game_score.mean()
+avg_puzzle_score = pd.DataFrame(puzzle_ic_by_gender)
+to_tabulate(avg_puzzle_score, "psql")
+print("-------------------------------------------------\n")
 
-
-ic_by_gender = ic.groupby("gender").puzzle_game_score.mean()
-icdf2 = pd.DataFrame(ic_by_gender)
-to_tabulate(icdf, "psql")
-to_tabulate(icdf2, "psql")
+print("AVERAGE VIDEO GAME SCORES\n")
+video_avg_by_gender = ic.groupby("gender").video_game_score.mean()
+avg_video_score = pd.DataFrame(video_avg_by_gender)
+to_tabulate(avg_video_score, "psql")
+print("-------------------------------------------------")
 
 # unpivot_ic = ic.groupby(["gender", "person_id"])["video_game_score"].mean().reset_index()
 #
