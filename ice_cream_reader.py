76a668662286ad54cff809bf6ca8948359df4a8d
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-15 00:27:51 -0500

    used tabluate library on my pivot

--- ice_cream_reader.py
+++ ice_cream_reader.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 # Reading the CSV file using the pandas module
 ic = pd.read_csv("ice_cream.csv")
+# print(ic.head(10))
 # Changed all column names using the column method so that they are more clear
 ic.columns = ["person_id", "gender", "ice_cream_flavor", "video_game", "puzzle_game"]
 # I replaced the 0 and 1 contents in the gender column to say male and female using the replace method
@@ -13,14 +14,19 @@ ic["ice_cream_flavor"] = ic["ice_cream_flavor"].replace({1: 'vanilla', 2: 'choco
 # set a variable to hold the dataframe method being used on the csv file ic
 icdf = pd.DataFrame(ic)
 
-# using the tabulate function that takes a dataframe as the first parameter, the header: the name of the columns, and
-# the third parameter is the table style or format
+# Using the tabulate library that takes a Pandas DataFrame as the first parameter, the second parameter the header: it
+# takes the column names (keys) and uses them as headers in the table and the third parameter specifies the fromat
+# of the table which is set to "psql"
 print(tabulate(icdf, headers="keys", tablefmt="psql"))
 
-unpivot_ic = ic.groupby(["gender", "puzzle_game"]) ["video_game"].mean().reset_index()
 
-pivoted_ic = unpivot_ic.pivot(columns="puzzle_game",
-                      index="gender",
-                      values="video_game"
-                      )
+unpivot_ic = ic.groupby(["gender", "puzzle_game"])["video_game"].mean().reset_index()
+
+pivoted_ic = unpivot_ic.pivot(
+    columns="puzzle_game",
+    index="gender",
+    values="video_game"
+)
+
 print(tabulate(pivoted_ic, headers="keys", tablefmt="psql"))
+
