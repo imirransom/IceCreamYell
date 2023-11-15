98b3fc2280726163c7f6bd65669339c0afa4b1cb
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-15 10:05:47 -0500

    adding a function for the tabulate library

--- ice_cream_reader.py
+++ ice_cream_reader.py
@@ -6,27 +6,40 @@ import pandas as pd
 ic = pd.read_csv("ice_cream.csv")
 # print(ic.head(10))
 # Changed all column names using the column method so that they are more clear
-ic.columns = ["person_id", "gender", "ice_cream_flavor", "video_game", "puzzle_game"]
+ic.columns = ["person_id", "gender", "ice_cream_flavor", "video_game_score", "puzzle_game_score"]
 # I replaced the 0 and 1 contents in the gender column to say male and female using the replace method
 ic['gender'] = ic['gender'].replace({0: 'male', 1: 'female'})
 # Used the replace method to display the flavored ice cream rather than the numbers 1, 2, and 3
 ic["ice_cream_flavor"] = ic["ice_cream_flavor"].replace({1: 'vanilla', 2: 'chocolate', 3: "strawberry"})
 # set a variable to hold the dataframe method being used on the csv file ic
 icdf = pd.DataFrame(ic)
-
+print("Info on ice cream table\n")
+print("-------------------------------------------------")
+print(ic.info())
+print("-------------------------------------------------\n")
 # Using the tabulate library that takes a Pandas DataFrame as the first parameter, the second parameter the header: it
-# takes the column names (keys) and uses them as headers in the table and the third parameter specifies the fromat
+# takes the column names (keys) and uses them as headers in the table and the third parameter specifies the format
 # of the table which is set to "psql"
-print(tabulate(icdf, headers="keys", tablefmt="psql"))
+def to_tabulate(df, format):
+    print(tabulate(df, headers="keys", tablefmt=format))
+
+# print(tabulate(icdf.head(20), headers="keys", tablefmt="psql"))
 
 
-unpivot_ic = ic.groupby(["gender", "puzzle_game"])["video_game"].mean().reset_index()
+to_tabulate(icdf.head(20), "psql")
 
-pivoted_ic = unpivot_ic.pivot(
-    columns="puzzle_game",
-    index="gender",
-    values="video_game"
-)
 
-print(tabulate(pivoted_ic, headers="keys", tablefmt="psql"))
+ic_by_gender = ic.groupby("gender").puzzle_game_score.mean()
+
+to_tabulate(ic_by_gender, "psql")
+
+# unpivot_ic = ic.groupby(["gender", "person_id"])["video_game_score"].mean().reset_index()
+#
+# pivoted_ic = unpivot_ic.pivot(
+#     columns="person_id",
+#     index="gender",
+#     values="video_game_score"
+# )
+#
+# print(tabulate(pivoted_ic, headers="keys", tablefmt="psql"))
 
